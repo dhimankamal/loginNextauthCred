af076b6bc35c133f60341382b59f9d959e7757ee
kamal <dhimank079@gmail.com>
2022-10-09 08:29:46 +0530

    update get single service

--- pages/api/services/getSingleService.js
+++ pages/api/services/getSingleService.js
@@ -2,12 +2,18 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function handle (req, res) {
-  const { body } = await req
-  const service = await prisma.services.findUnique({
-    where: {
-      id: body?.id
-    }
-  })
-  console.log('services', service)
+  //const { body } = await req
+  let { id } = await JSON.parse(req.body)
+  console.log('id++++2233', id)
+  let service = {}
+  if (id) {
+     service = await prisma.services.findUnique({
+      where: {
+        id
+      }
+    })
+  }
+  console.log(service)
   res.json(service)
+  // console.log('services', service)
 }
--- pages/services/[id].js
+++ pages/services/[id].js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 export default function ServicePage () {
   const router = useRouter()
   const { id } = router.query
+
+  const getServiceDetail = async () => {
+    try {
+      const res = await fetch('/api/services/getSingleService', {
+        method: 'POST',
+        body: JSON.stringify({id})
+      }) 
+      const data = await res.json()
+
+      console.log("res", data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getServiceDetail()
+  }, [id])
   return (
     <main className='w-screen text-center py-10'>
       <h2 className='text-2xl'>Id = {id}</h2>
@@ -17,7 +35,12 @@ export default function ServicePage () {
           <li>Test : Test 2</li>
           <li>Test : Test 2</li>
         </ul>
-        <button className='bg-gray-700 px-10 py-4 rounded text-xl text-white font-bold my-10' onClick={()=>alert('clicked')}>Print Pdf</button>
+        <button
+          className='bg-gray-700 px-10 py-4 rounded text-xl text-white font-bold my-10'
+          onClick={() => alert('clicked')}
+        >
+          Print Pdf
+        </button>
       </div>
     </main>
   )
