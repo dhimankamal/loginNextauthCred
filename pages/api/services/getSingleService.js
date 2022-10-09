7bdab49ced37f984ea9cddcf927feaaa9c030fc8
kamal <dhimank079@gmail.com>
2022-10-09 10:50:38 +0530

    add data service

--- pages/api/services/getSingleService.js
+++ pages/api/services/getSingleService.js
@@ -2,9 +2,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function handle (req, res) {
-  //const { body } = await req
   let { id } = await JSON.parse(req.body)
-  console.log('id++++2233', id)
   let service = {}
   if (id) {
      service = await prisma.services.findUnique({
@@ -13,7 +11,5 @@ export default async function handle (req, res) {
       }
     })
   }
-  console.log(service)
   res.json(service)
-  // console.log('services', service)
 }
--- pages/services/[id].js
+++ pages/services/[id].js
@@ -1,43 +1,72 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import { getSession } from 'next-auth/react'
 
 export default function ServicePage () {
   const router = useRouter()
   const { id } = router.query
-
+  const [fields, setFields] = useState(['loading'])
   const getServiceDetail = async () => {
     try {
       const res = await fetch('/api/services/getSingleService', {
         method: 'POST',
-        body: JSON.stringify({id})
-      }) 
+        body: JSON.stringify({ id })
+      })
       const data = await res.json()
-
-      console.log("res", data)
+      if (data && data.fields) {
+        setFields(JSON.parse(data.fields))
+      }
     } catch (error) {
       console.log(error)
     }
   }
 
-  useEffect(() => {
+  const [profileData, setProfileData] = useState({})
+
+  const getUSerData = async () => {
+    const getUserSession = await getSession()
+    if (getUserSession) {
+      try {
+        const res = await fetch('/api/getUser', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(getUserSession?.user)
+        })
+        const data = await res.json()
+        setProfileData(data)
+        console.log('user data', data)
+      } catch (error) {
+        console.log('error', error)
+      }
+    }
+  }
+
+  useEffect(async () => {
+    await getUSerData()
     getServiceDetail()
   }, [id])
+
+  useEffect(() => {
+    console.log(profileData, fields)
+  }, [fields])
   return (
     <main className='w-screen text-center py-10'>
-      <h2 className='text-2xl'>Id = {id}</h2>
+      <h2 className='text-2xl'>Service id = {id}</h2>
       <div className='py-10'>
         <ul className='text-xl space-y-4'>
-          <li>Name : kamal</li>
-          <li>Test : Test 2</li>
-          <li>Test : Test 2</li>
-          <li>Test : Test 2</li>
-          <li>Test : Test 2</li>
-          <li>Test : Test 2</li>
-          <li>Test : Test 2</li>
+          {fields &&
+            fields.map(value => (
+              <li key={value} className='capitalize'>
+                {value} : {profileData[value]}
+              </li>
+            ))}
         </ul>
         <button
-          className='bg-gray-700 px-10 py-4 rounded text-xl text-white font-bold my-10'
+          className='bg-gray-700 px-10 py-4 rounded text-xl text-white font-bold my-10 disabled:opacity-50'
           onClick={() => alert('clicked')}
+          disabled={fields[0] === 'loading'}
         >
           Print Pdf
         </button>
