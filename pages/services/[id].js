8615dae022a21eab7c54cf4547a30995a7998289
kamal <dhimank079@gmail.com>
2022-10-29 18:18:19 +0530

    add form

--- pages/services/[id].js
+++ pages/services/[id].js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { getSession } from 'next-auth/react'
 import { PDFDocument } from 'pdf-lib'
+import { Formik, Field, ErrorMessage, Form } from 'formik'
 
 export default function ServicePage () {
   const router = useRouter()
+  const [error, setError] = useState(null)
   const { id } = router.query
   const [fields, setFields] = useState(['loading'])
   const [profileData, setProfileData] = useState({})
@@ -45,7 +47,7 @@ export default function ServicePage () {
     }
   }
 
-  const getForm = async () => {
+  const getForm = async data => {
     console.log('process.env+++', process.env.NEXT_PUBLIC_NEXTAUTH_URL)
     const formUrl = `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}aadhaar.pdf`
     const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer())
@@ -53,8 +55,8 @@ export default function ServicePage () {
     const form = pdfDoc.getForm()
     //const fields = form.getFields()
     console.log('profileData', profileData)
-    form.getTextField('Text-ODjXylvMS3').setText(profileData.name)
-    form.getTextField('Text-vroy5YIQGU').setText(profileData.address)
+    form.getTextField('Text-ODjXylvMS3').setText(data.fullname)
+    form.getTextField('Text-vroy5YIQGU').setText(data.address)
 
     // fields.forEach(field => {
     //   const type = field.constructor.name
@@ -82,26 +84,76 @@ export default function ServicePage () {
     console.log(profileData, fields)
   }, [fields])
   return (
-    <main className='w-screen text-center py-10'>
-      <h2 className='text-2xl'>Service id = {id}</h2>
+    <main className='w-screen text-left py-10'>
       <div className='py-10'>
-        <ul className='text-xl space-y-4'>
-          {fields &&
-            fields.map(value => (
-              <li key={value} className='capitalize'>
-                {value} : {profileData[value]}
-              </li>
-            ))}
-        </ul>
-        <button
-          className='bg-gray-700 px-10 py-4 rounded text-xl text-white font-bold my-10 disabled:opacity-50'
-          onClick={async () => {
-            getForm()
+        <Formik
+          initialValues={{
+            fullname: '',
+            address: ''
           }}
-          disabled={fields[0] === 'loading'}
+          onSubmit={async values => {
+            console.log('values___', values)
+            getForm(values)
+          }}
         >
-          Print Pdf
-        </button>
+          <Form>
+            <div className='bg-white flex flex-col items-center justify-center py-2 shadow-lg'>
+              <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-4/5'>
+                <h2 className='text-2xl'>Service id = {id}</h2>
+                <div className='text-red-400 text-md text-center rounded p-2'>
+                  {error}
+                </div>
+                <div className='mb-4'>
+                  <label
+                    htmlFor='fullname'
+                    className='uppercase text-sm text-gray-600 font-bold'
+                  >
+                    Name
+                    <Field
+                      name='fullname'
+                      aria-label='enter your name'
+                      aria-required='true'
+                      type='text'
+                      className='w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline'
+                    />
+                  </label>
+
+                  <div className='text-red-600 text-sm'>
+                    <ErrorMessage name='fullname' />
+                  </div>
+                </div>
+                <div className='mb-4'>
+                  <label
+                    htmlFor='fullname'
+                    className='uppercase text-sm text-gray-600 font-bold'
+                  >
+                    Address
+                    <Field
+                      name='address'
+                      aria-label='enter your name'
+                      aria-required='true'
+                      type='text'
+                      className='w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline'
+                    />
+                  </label>
+
+                  <div className='text-red-600 text-sm'>
+                    <ErrorMessage name='address' />
+                  </div>
+                </div>
+
+                <div className='flex items-center justify-center'>
+                  <button
+                    type='submit'
+                    className='uppercase text-sm font-bold tracking-wide bg-green-400 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline hover:shadow-xl active:scale-90 transition duration-150'
+                  >
+                    Print Now
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Form>
+        </Formik>
       </div>
     </main>
   )
